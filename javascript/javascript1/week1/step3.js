b5274582b6f69970ab154b48ee94051423dcccdd
Gizem Candemir <gizemc@gmail.com>
2019-09-06 23:32:39 +0200

    add house price check app

--- javascript/javascript1/week1/step3.js
+++ javascript/javascript1/week1/step3.js
@@ -15,3 +15,20 @@ const shouldShowResultInDogYears = dogYear * 7;
 console.log("Your dog will be ", shouldShowResultInDogYears, "dog years old in 2027")
 
 
+// Housey pricey (A house price estimator)
+
+const volumeInMetersPeter = 8 * 10 * 10;
+const gardenSizeInM2Peter = 100;
+const paymentPeter = 2500000;
+const housePricePeter = volumeInMetersPeter * 2.5 * 1000 + gardenSizeInM2Peter * 300;
+
+console.log("Peter payed too much : ", paymentPeter > housePricePeter);
+
+const volumeInMetersJulia = 5 * 11 * 8;
+const gardenSizeInM2Julia = 70;
+const paymentJulia = 1000000;
+const housePriceJulia = volumeInMetersJulia * 2.5 * 1000 + gardenSizeInM2Julia * 300;
+
+console.log("Julia payed too much : ", paymentJulia > housePriceJulia);
+
+
