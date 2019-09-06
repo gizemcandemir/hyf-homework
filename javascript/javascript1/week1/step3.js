41346e23f729c0c1a7bdb322d4fac30f309d236c
Gizem Candemir <gizemc@gmail.com>
2019-09-06 23:32:16 +0200

    add dog age calculator

---
+++ javascript/javascript1/week1/step3.js
@@ -0,0 +1,17 @@
+// Age-ify (A future age calculator)
+
+const yearOfBirth = 1982;
+const yearFuture = 2045;
+const age = yearFuture - yearOfBirth;
+console.log("You will be ", age, " years old in 2027");
+
+
+// Goodboy-Oldboy (A dog age calculator)
+
+const dogYearOfBirth = 2017;
+const dogYearFuture = 2027;
+const dogYear = dogYearFuture - dogYearOfBirth;
+const shouldShowResultInDogYears = dogYear * 7;
+console.log("Your dog will be ", shouldShowResultInDogYears, "dog years old in 2027")
+
+
