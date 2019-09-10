2944340612aaa846be3945560ac88ddde6eeaa4d
Gizem Candemir <gizemc@gmail.com>
2019-09-10 12:31:30 +0200

    getFullName function with optional FormalName

--- javascript/javascript1/week2/step3.js
+++ javascript/javascript1/week2/step3.js
@@ -1,3 +1,20 @@
-function getFullName (firstname, surname) {
-  return (firstname + surname);
+function getFullName (firstName, surname, useFormalName) {
+  if (useFormalName == undefined) {
+    const fullName = firstName + " " + surname;  
+    return (fullName);
+  } else if (useFormalName == true) {
+    const fullName = "Lord " + firstName + " " + surname;  
+    return (fullName);
+  } else {
+    const fullName = firstName + " " + surname;
+    return (fullName);
+  }
 }
+
+const fullName1 = getFullName("Gizem", "Candemir", true);
+const fullName2 = getFullName("Jane", "Smith", false);
+const fullName3 = getFullName("Oya", "Kilicoglu");
+
+console.log(fullName1);
+console.log(fullName2);
+console.log(fullName3);
