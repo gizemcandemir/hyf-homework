1a0148175037d85e85a02e592c2c49b00a1d25f0
Gizem Candemir <gizemc@gmail.com>
2019-09-12 06:22:03 +0200

    getEventDay  application

--- javascript/javascript1/week2/eventApplication.js
+++ javascript/javascript1/week2/eventApplication.js
@@ -1,6 +1,15 @@
-const now = new Date();
+const weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
-// View the output
-now;
+const today = new Date();
+console.log("Today is " + weekDays[today.getDay()-1] + ".");
 
-console.log(now);
+function getEventWeekday(num) {
+  const eventDay = today.getDay() - 1 + num 
+  if (eventDay >= 7) {
+    console.log ("The event will be on a " + weekDays[(eventDay % 7)] + ".");
+  } else {
+    console.log ("The event will be on a " + weekDays[(eventDay)] + ".");
+  }
+}
+
+return getEventWeekday(3);
