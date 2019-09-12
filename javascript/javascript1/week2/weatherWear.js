e3e2889a3342bb6f4ca70211a78fa0a6ef7a6c07
Gizem Candemir <gizemc@gmail.com>
2019-09-13 00:33:16 +0200

    write function

---
+++ javascript/javascript1/week2/weatherWear.js
@@ -0,0 +1,23 @@
+function whatToWear(degree) {
+  const clothesToWear = ["Swimsuit", "T-shirt", "Sweater", "Jacket", "Snow-jacket"];
+  const text = "It's " + degree + " degrees, you should wear: ";
+  if (degree > 30 ) {
+    return text + clothesToWear[0];
+  } else if (degree > 23) {
+    return text + clothesToWear[1];
+  } else if (degree > 13) {
+    return text + clothesToWear[2];
+  } else if (degree > 0) {
+    return text + clothesToWear[3];
+  } else if (degree <= 0) {
+    return text + clothesToWear[4];
+  } else {
+    return "and I don't know what to wear?";
+  }
+}
+
+console.log(whatToWear(-5));
+console.log(whatToWear(13));
+console.log(whatToWear(21));
+console.log(whatToWear(28));
+console.log(whatToWear(33));
