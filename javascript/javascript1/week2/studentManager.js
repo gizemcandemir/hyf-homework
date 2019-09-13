48d99403be27db99e602e4d9d5a366b4e1c00fcd
Gizem Candemir <gizemc@gmail.com>
2019-09-13 02:00:48 +0200

    addStudentToClass function

---
+++ javascript/javascript1/week2/studentManager.js
@@ -0,0 +1,27 @@
+const class07Students = [];
+
+function addStudentToClass(studentName) {
+  for (i=0; i<class07Students.length; i++) {
+    if (studentName == class07Students[i]) {
+      return "Student " + studentName + " is already in the class.";
+    }
+  }
+
+  if (class07Students.length < 6 || studentName == "Margrethe II") {
+    return "Student " + studentName + " is registered to the class with ID: " + class07Students.push(studentName);
+  } else {
+    return "Can not add more students to this class.";
+  }
+}
+
+console.log(addStudentToClass("Gizem"));
+console.log(addStudentToClass("Dara"));
+console.log(addStudentToClass("Oya"));
+console.log(addStudentToClass("Oya"));
+console.log(addStudentToClass("Jane"));
+console.log(addStudentToClass("John"));
+console.log(addStudentToClass("Joe"));
+console.log(addStudentToClass("Joe"));
+console.log(addStudentToClass("Jack"));
+console.log(addStudentToClass("Margrethe II"));
+console.log(class07Students);
