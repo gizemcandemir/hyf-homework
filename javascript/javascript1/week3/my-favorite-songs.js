9684ce258072fa2ef6a431313e20a6a3539132b0
Gizem Candemir <gizemc@gmail.com>
2019-09-19 22:08:32 +0200

    add songs to database

--- javascript/javascript1/week3/my-favorite-songs.js
+++ javascript/javascript1/week3/my-favorite-songs.js
@@ -21,3 +21,34 @@ const songDatabase = [{
 ];
 
 const myPlaylist = [];
+
+const song = [{
+  songId: songDatabase.length+1,
+  title: 'Gunes Yerinde',
+  artist: 'Buyuk Ev Ablukada',
+},
+{
+  songId: songDatabase.length+2,
+  title: 'Ya Sonra',
+  artist: 'Ajda Pekkan',
+},
+{
+  songId: songDatabase.length+3,
+  title: 'Hal Hal',
+  artist: 'Baris Manco',
+},
+]
+
+// console.log(song);
+
+function addSongToDatabase(newSong) {
+  for (let i=0; i<newSong.length; i++) {
+    songDatabase.push(newSong[i]);
+  }
+}
+
+addSongToDatabase(song); 
+
+// I think this function doesn't need to return anything, since it's doing its job by pushing the new object's keys&values but just to see the updated database i use console.log to see the current database.
+
+console.log(songDatabase);
