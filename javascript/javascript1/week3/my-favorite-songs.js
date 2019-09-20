d6e14ea2ba026a9934c86837a3a8e742ac689fd2
Gizem Candemir <gizemc@gmail.com>
2019-09-20 23:57:41 +0200

    add addSongToMyPlaylist function

--- javascript/javascript1/week3/my-favorite-songs.js
+++ javascript/javascript1/week3/my-favorite-songs.js
@@ -1,22 +1,22 @@
 const songDatabase = [{
   songId: 1,
   title: 'My baby',
-  artist: 'Soggy socks',
+  artist: 'Soggy socks'
 },
 {
   songId: 2,
   title: '3 nails in wood',
-  artist: 'The carpenters',
+  artist: 'The carpenters'
 },
 {
   songId: 3,
   title: 'Blacker than black',
-  artist: 'Instant coffee',
+  artist: 'Instant coffee'
 },
 {
   songId: 4,
   title: 'When is enough too little?',
-  artist: 'The spies girls',
+  artist: 'The spies girls'
 },
 ];
 
@@ -25,23 +25,18 @@ const myPlaylist = [];
 const favorites = [{
   songId: 5,
   title: 'Gunes Yerinde',
-  artist: 'Buyuk Ev Ablukada',
+  artist: 'Buyuk Ev Ablukada'
 },
 {
   songId: 6,
   title: 'Ya Sonra',
-  artist: 'Ajda Pekkan',
+  artist: 'Ajda Pekkan'
 },
 {
   songId: 7,
   title: 'Hal Hal',
-  artist: 'Baris Manco',
-},
-{
-  songId: 8,
-  title: 'Hal Hal',
-  artist: 'Sezen Aksu',
-},
+  artist: 'Baris Manco'
+}
 ]
 
 // we can see the current database:
@@ -60,48 +55,49 @@ function addSongsToDatabase(songs) {
   addedSongs = [];
   for (let i=0; i<songs.length; i++) {
     addedSongs.push(addSongToDatabase(songs[i]));
-    console.log(`'${addedSongs[addedSongs.length-1].title}' added to the database.`);
+    // console.log(`'${addedSongs[addedSongs.length-1].title}' added to the database.`);
   }
-  console.log(`\nTotal of ${addedSongs.length} songs added to the database.\n`);  
+  // console.log(`\nTotal of ${addedSongs.length} songs added to the database.\n`);  
   return addedSongs;
 }
 
 addSongsToDatabase(favorites)
-
 // console.log(songDatabase);
-// [
-//   { songId: 1, title: 'My baby', artist: 'Soggy socks' },
-//   { songId: 2, title: '3 nails in wood', artist: 'The carpenters' },
-//   { songId: 3, title: 'Blacker than black', artist: 'Instant coffee' },
-//   {
-//     songId: 4,
-//     title: 'When is enough too little?',
-//     artist: 'The spies girls'
-//   },
-//   { songId: 5, title: 'Gunes Yerinde', artist: 'Buyuk Ev Ablukada' },
-//   { songId: 6, title: 'Ya Sonra', artist: 'Ajda Pekkan' },
-//   { songId: 7, title: 'Hal Hal', artist: 'Baris Manco' }
-// ]
-
 
 function getSongByTitle(title) {
-  foundTitle = false;
+  let song = undefined;
   const regex = new RegExp(title, 'i');
 
   for (let i=0; i<songDatabase.length; i++) {
     if (songDatabase[i].title.match(regex)) {
-      foundTitle = true;
-      console.log(`Found '${title}' in ${songDatabase[i].title}`);
+      song = songDatabase[i];
+      console.log(`Found '${title}' in ${song.title} by ${song.artist}`);
+      return song;
     } 
   } 
 
-  if (!foundTitle) {
-    console.log(`Found '${title}' could not be found.`);
+  if (!song) {
+    console.log(`'${title}' could not be found.`);
+    return song;
   }
-  
-  return;
 }
 
-getSongByTitle('Hal Hal');
-getSongByTitle('hal');
-getSongByTitle('Ya Sonra');
+// getSongByTitle('Hal Hal');
+// getSongByTitle('hal');
+// getSongByTitle('Ya Sonra');
+
+function addSongToMyPlaylist(title) {
+  song = getSongByTitle(title);
+  
+  if (song) { 
+    myPlaylist.push(song);
+  }
+  
+  return myPlaylist;
+}
+
+addSongToMyPlaylist('Hal Hal');
+addSongToMyPlaylist('Best Song Ever');
+addSongToMyPlaylist('ya sonra');
+
+console.log(myPlaylist);
