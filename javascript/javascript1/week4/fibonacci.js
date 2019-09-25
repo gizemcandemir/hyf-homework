e23ec11d18eb3a1f5a06c17fc7c9457792b5cd5b
Gizem Candemir <gizemc@gmail.com>
2019-09-25 11:21:40 +0200

    remove unnecessary push

--- javascript/javascript1/week4/fibonacci.js
+++ javascript/javascript1/week4/fibonacci.js
@@ -1,13 +1,14 @@
 const fibonacciNumbers = [];
 
-function fibonacci(num){
-  for (let i = 2; i < num; i++){
+function fib(num){
+  for (let i = 2 ; i < num; i++){
     fibonacciNumbers[0] = 0;
-    fibonacciNumbers[1] = 1;
+    fibonacciNumbers[1] = 1;    
     fibonacciNumbers[i] = fibonacciNumbers[i-1] + fibonacciNumbers[i-2];
-    fibonacciNumbers.push(fibonacciNumbers[i]);
-  } return fibonacciNumbers;
+  } return fibonacciNumbers[fibonacciNumbers.length-1];
 }
 
-console.log(fibonacci(5));
-// [ 0, 1, 1, 2, 3, 3 ] why does it repeat the last element??
+console.log(fib(5));
+console.log(fib(10));
+
+// console.log(fibonacciNumbers);
