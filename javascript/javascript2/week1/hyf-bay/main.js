d8f525856f233630e6da23642914e136ba977e03
Gizem Candemir <gizemc@gmail.com>
2019-10-04 23:41:55 +0200

    Complete task

--- javascript/javascript2/week1/hyf-bay/main.js
+++ javascript/javascript2/week1/hyf-bay/main.js
@@ -1,29 +1,51 @@
 console.log("Script loaded");
-
 const testProductNames = ["Flat screen", "Mobile phone", "Wallet"];
 
-const productList = document.getElementsByClassName("products")[0];
-// const productList = document.querySelector("ul");
-
 function renderProducts(array) {
-    for (let i = 0; i < array.length; i++) {
-        const product = document.createElement("li");
-        product.innerHTML = `${array[i].name} | ${array[i].price} | ${array[i].rating} | ${array[i].shipsTo}`;
-        productList.appendChild(product);
-    } 
-        // const productDetails = productList.appendChild(product); 
-        // const ul = product.createElement("ul");
-        // const li = ul.createElement("li");
-        // console.log(ul);
-        // console.log(li);
-        
-            // for (let i = 0; i < array.length; i++) {
-            //     product.ul.appendChild(li);
-            // }
-        // product.ul.appendChild(li);
+  const productList = document.querySelector(".products > ul");
+  for (let i = 0; i < array.length; i++) {
+    const product = array[i];
+    const productLi = document.createElement("li");
+    productList.appendChild(productLi);
+    productLi.appendChild(renderProduct(product));
+  }
+}
+
+function renderProduct(product) {
+  const productDetails = document.createElement("ul");
+
+  const nameLi = document.createElement("li");
+  nameLi.innerText = product.name;
+  nameLi.classList.add("name");
+  productDetails.appendChild(nameLi);
+
+  const priceLi = document.createElement("li");
+  priceLi.innerText = product.price;
+  priceLi.classList.add("price");
+  productDetails.appendChild(priceLi);
+
+  const ratingLi = document.createElement("li");
+  ratingLi.innerText = product.rating;
+  ratingLi.classList.add("rating");
+  productDetails.appendChild(ratingLi);
+
+  const shipsToLi = document.createElement("li");
+  shipsToLi.classList.add("shipsTo");
+  shipsToLi.appendChild(renderShipsTo(product.shipsTo));
+  productDetails.appendChild(shipsToLi);
+
+  return productDetails;
+}
+
+function renderShipsTo(array) {
+  const shipsToList = document.createElement("ul");
+  for (let i = 0; i < array.length; i++) {
+    const countryLi = document.createElement("li");
+    countryLi.innerText = array[i];
+    shipsToList.appendChild(countryLi);
+  }
+  return shipsToList;
 }
 
 const products = getAvailableProducts();
-console.log(products);
-
-console.log(renderProducts(products));
+renderProducts(products);
