5270136716aad4cc774fbfdfd3dd95247c90d2b4
Gizem Candemir <gizemc@gmail.com>
2019-10-08 22:34:16 +0200

    Run function with map and filter

---
+++ javascript/javascript2/week2/doubling-of-number/main.js
@@ -0,0 +1,6 @@
+let numbers = [1, 2, 3, 4];
+let newNumbers = [];
+
+newNumbers = numbers.filter(number => number % 2 !== 0).map(number => number * 2);
+
+console.log("The doubled numbers are", newNumbers); // [2, 6]
