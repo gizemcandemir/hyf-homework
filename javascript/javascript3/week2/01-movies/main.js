b1fb5791566f6b2f66bd5898dcc94701d22d44c6
Gizem Candemir <gizemc@gmail.com>
2019-10-28 11:13:20 +0100

    Create defined arrays from the movies api

--- javascript/javascript3/week2/01-movies/main.js
+++ javascript/javascript3/week2/01-movies/main.js
@@ -5,4 +5,14 @@ fetch(url)
 	.then(value => value.json())
 	.then(movies => {
 		const badMovies = movies.filter(movie => movie.rating < 4);
+		return badMovies;
+	})
+	.then(movies => {
+		const badMoviesSince2000 = movies.filter(movie => movie.year >= 2000);
+		return badMoviesSince2000;
+	})
+	.then(movies => {
+		const badMovieTitlesSince2000 = movies.map(movie => movie.title);
+		console.log(badMovieTitlesSince2000);
+		return badMovieTitlesSince2000;
 	});
