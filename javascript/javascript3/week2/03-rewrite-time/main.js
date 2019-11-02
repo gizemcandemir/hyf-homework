22de7850018d903644ab5907c9dd701857151b7b
Gizem Candemir <gizemc@gmail.com>
2019-11-02 22:18:44 +0100

    Rewrite functions with promise

---
+++ javascript/javascript3/week2/03-rewrite-time/main.js
@@ -0,0 +1,27 @@
+// Rewrite setTimeout and navigator.geolocation.getCurrentPosition to promises. So instead of using setTimeout and navigator.geolocation.getCurrentPosition with callbacks, use it as a promise.
+
+function setTimeoutPromise(miliseconds) {
+	return new Promise(resolve => {
+		setTimeout(() => resolve(), miliseconds);
+	});
+}
+
+setTimeoutPromise(3000).then(() => {
+	console.log(`Called after 3 seconds`);
+});
+
+function getCurrentLocation() {
+	return new Promise((resolve, reject) => {
+		navigator.geolocation.getCurrentPosition(resolve, reject)
+	})
+}		
+
+getCurrentLocation()
+	.then(position => {
+		// called when the users position is found
+		console.log(position);
+	})
+	.catch(error => {
+		// called if there was an error getting the users location
+		console.log(error);
+	});
