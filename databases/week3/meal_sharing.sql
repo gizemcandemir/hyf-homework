761c9aba58366e13a278d980c8c455f07c18f0c9
Gizem Candemir <gizemc@gmail.com>
2019-11-28 23:40:22 +0100

    Update indentations

--- databases/week3/meal_sharing.sql
+++ databases/week3/meal_sharing.sql
@@ -5,34 +5,34 @@ USE meal_sharing;
 
 CREATE TABLE `meal` (
 	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `title` VARCHAR(255) NOT NULL,
-    `description` TEXT,
-    `location` VARCHAR(255) NOT NULL,
-    `when` DATETIME NOT NULL,
-    `max_reservations` INT UNSIGNED NOT NULL,
-    `price` DECIMAL NOT NULL,
-    `created_date` DATE NOT NULL,
-    PRIMARY KEY (id)
+	`title` VARCHAR(255) NOT NULL,
+	`description` TEXT,
+	`location` VARCHAR(255) NOT NULL,
+	`when` DATETIME NOT NULL,
+	`max_reservations` INT UNSIGNED NOT NULL,
+	`price` DECIMAL NOT NULL,
+	`created_date` DATE NOT NULL,
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE `reservation` (
 	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `number_of_guests` INT UNSIGNED NOT NULL,
-    `meal_id` INT UNSIGNED, 
-    `created_date` DATE NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (meal_id) REFERENCES `meal` (id)
+	`number_of_guests` INT UNSIGNED NOT NULL,
+	`meal_id` INT UNSIGNED, 
+	`created_date` DATE NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (meal_id) REFERENCES `meal` (id)
 );
 
 CREATE TABLE `review` (
 	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `title` VARCHAR(255) NOT NULL,
-    `description` TEXT,
-    `meal_id` INT UNSIGNED,
-    `stars` INT UNSIGNED,
-    `created_date` DATE NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (meal_id) REFERENCES `meal` (id)
+	`title` VARCHAR(255) NOT NULL,
+	`description` TEXT,
+	`meal_id` INT UNSIGNED,
+	`stars` INT UNSIGNED,
+	`created_date` DATE NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (meal_id) REFERENCES `meal` (id)
 );
 
 -- [MEAL]
@@ -42,18 +42,18 @@ SELECT * from meal;
 -- Add a new meal
 INSERT INTO `meal` (
 	`title`,
-    `location`,
-    `when`,
-    `max_reservations`,
-    `price`,
-    `created_date`
+	`location`,
+	`when`,
+	`max_reservations`,
+	`price`,
+	`created_date`
 ) VALUES (
 	"Turkish brunch",
-    "Nørrebro",
-    "2019-12-01",
-    4,
-    25,
-    NOW()
+	"Nørrebro",
+	"2019-12-01",
+	4,
+	25,
+	NOW()
 );
 
 -- Get a meal with any id, fx 1
@@ -138,280 +138,280 @@ WHERE id = 1;
 -- [ADDITIONAL QUERIES]
 INSERT INTO `meal` (
 	`title`,
-    `description`,
-    `location`,
-    `when`,
-    `max_reservations`,
-    `price`,
-    `created_date`
+	`description`,
+	`location`,
+	`when`,
+	`max_reservations`,
+	`price`,
+	`created_date`
 ) VALUES (
 	"Danish brunch",
-    "Sausages, omlette and pancakes",
-    "Østerbro",
-    "2019-12-05",
-    4,
-    120,
-    NOW()
+	"Sausages, omlette and pancakes",
+	"Østerbro",
+	"2019-12-05",
+	4,
+	120,
+	NOW()
 );
 
 INSERT INTO `meal` (
 	`title`,
-    `description`,
-    `location`,
-    `when`,
-    `max_reservations`,
-    `price`,
-    `created_date`
+	`description`,
+	`location`,
+	`when`,
+	`max_reservations`,
+	`price`,
+	`created_date`
 ) VALUES (
 	"Vegan brunch",
-    "Oatmeal grød with almond milk",
-    "Vesterbro",
-    "2019-12-05",
-    4,
-    99,
-    NOW()
+	"Oatmeal grød with almond milk",
+	"Vesterbro",
+	"2019-12-05",
+	4,
+	99,
+	NOW()
 );
 
 INSERT INTO `meal` (
 	`title`,
-    `description`,
-    `location`,
-    `when`,
-    `max_reservations`,
-    `price`,
-    `created_date`
+	`description`,
+	`location`,
+	`when`,
+	`max_reservations`,
+	`price`,
+	`created_date`
 ) VALUES (
 	"Danish brunch",
-    "Sausages, omlette and pancakes",
-    "Østerbro",
-    "2019-11-25",
-    4,
-    120,
-    "2019-11-25"
+	"Sausages, omlette and pancakes",
+	"Østerbro",
+	"2019-11-25",
+	4,
+	120,
+	"2019-11-25"
 );
 
 INSERT INTO `meal` (
 	`title`,
-    `description`,
-    `location`,
-    `when`,
-    `max_reservations`,
-    `price`,
-    `created_date`
+	`description`,
+	`location`,
+	`when`,
+	`max_reservations`,
+	`price`,
+	`created_date`
 ) VALUES (
 	"Turkish brunch",
-    "Including 'sucuklu yumurta' and 'börek'",
-    "Nørrebro",
-    "2019-11-26",
-    4,
-    119,
-    "2019-11-26"
+	"Including 'sucuklu yumurta' and 'börek'",
+	"Nørrebro",
+	"2019-11-26",
+	4,
+	119,
+	"2019-11-26"
 );
 
 INSERT INTO `meal` (
 	`title`,
-    `description`,
-    `location`,
-    `when`,
-    `max_reservations`,
-    `price`,
-    `created_date`
+	`description`,
+	`location`,
+	`when`,
+	`max_reservations`,
+	`price`,
+	`created_date`
 ) VALUES (
 	"Vegan brunch",
-    "Oatmeal grød with almond milk",
-    "Vesterbro",
-    "2019-11-27",
-    2,
-    99,
-    "2019-11-27"
+	"Oatmeal grød with almond milk",
+	"Vesterbro",
+	"2019-11-27",
+	2,
+	99,
+	"2019-11-27"
 );
 
 INSERT INTO `meal` (
 	`title`,
-    `description`,
-    `location`,
-    `when`,
-    `max_reservations`,
-    `price`,
-    `created_date`
+	`description`,
+	`location`,
+	`when`,
+	`max_reservations`,
+	`price`,
+	`created_date`
 ) VALUES (
 	"Turkish brunch",
-    "Including 'sucuklu yumurta' and 'börek'",
-    "Nørrebro",
-    "2019-12-03",
-    4,
-    119,
-    "2019-11-26"
+	"Including 'sucuklu yumurta' and 'börek'",
+	"Nørrebro",
+	"2019-12-03",
+	4,
+	119,
+	"2019-11-26"
 );
 
 INSERT INTO `meal` (
 	`title`,
-    `description`,
-    `location`,
-    `when`,
-    `max_reservations`,
-    `price`,
-    `created_date`
+	`description`,
+	`location`,
+	`when`,
+	`max_reservations`,
+	`price`,
+	`created_date`
 ) VALUES (
 	"Turkish brunch",
-    "Including 'sucuklu yumurta' and 'börek'",
-    "Nørrebro",
-    "2019-12-02",
-    4,
-    119,
-    "2019-11-25"
+	"Including 'sucuklu yumurta' and 'börek'",
+	"Nørrebro",
+	"2019-12-02",
+	4,
+	119,
+	"2019-11-25"
 );
 
 INSERT INTO `meal` (
 	`title`,
-    `description`,
-    `location`,
-    `when`,
-    `max_reservations`,
-    `price`,
-    `created_date`
+	`description`,
+	`location`,
+	`when`,
+	`max_reservations`,
+	`price`,
+	`created_date`
 ) VALUES (
 	"Vegan brunch",
-    "Oatmeal grød with almond milk",
-    "Vesterbro",
-    "2019-12-02",
-    4,
-    99,
-    "2019-11-25"
+	"Oatmeal grød with almond milk",
+	"Vesterbro",
+	"2019-12-02",
+	4,
+	99,
+	"2019-11-25"
 );
 
 INSERT INTO `meal` (
 	`title`,
-    `description`,
-    `location`,
-    `when`,
-    `max_reservations`,
-    `price`,
-    `created_date`
+	`description`,
+	`location`,
+	`when`,
+	`max_reservations`,
+	`price`,
+	`created_date`
 ) VALUES (
 	"Danish brunch",
-    "Sausages, omlette and pancakes",
-    "Østerbro",
-    "2019-12-04",
-    4,
-    120,
-    "2019-11-25"
+	"Sausages, omlette and pancakes",
+	"Østerbro",
+	"2019-12-04",
+	4,
+	120,
+	"2019-11-25"
 );
 
 INSERT INTO `reservation` (
-    `number_of_guests`,
-    `meal_id`,
-    `created_date`
+	`number_of_guests`,
+	`meal_id`,
+	`created_date`
 ) VALUES (
-    2,
-    2,
-    "2019-11-27"
+	2,
+	2,
+	"2019-11-27"
 );
 
 INSERT INTO `reservation` (
-    `number_of_guests`,
-    `meal_id`,
-    `created_date`
+	`number_of_guests`,
+	`meal_id`,
+	`created_date`
 ) VALUES (
-    2,
-    3,
-    "2019-11-27"
+	2,
+	3,
+	"2019-11-27"
 );
 
 INSERT INTO `reservation` (
-    `number_of_guests`,
-    `meal_id`,
-    `created_date`
+	`number_of_guests`,
+	`meal_id`,
+	`created_date`
 ) VALUES (
-    4,
-    1,
-    "2019-11-25"
+	4,
+	1,
+	"2019-11-25"
 );
 
 INSERT INTO `reservation` (
-    `number_of_guests`,
-    `meal_id`,
-    `created_date`
+	`number_of_guests`,
+	`meal_id`,
+	`created_date`
 ) VALUES (
-    2,
-    2,
-    NOW()
+	2,
+	2,
+	NOW()
 );
 
 INSERT INTO `reservation` (
-    `number_of_guests`,
-    `meal_id`,
-    `created_date`
+	`number_of_guests`,
+	`meal_id`,
+	`created_date`
 ) VALUES (
-    4,
-    2,
-    "2019-12-01"
+	4,
+	2,
+	"2019-12-01"
 );
 
 INSERT INTO `reservation` (
-    `number_of_guests`,
-    `meal_id`,
-    `created_date`
+	`number_of_guests`,
+	`meal_id`,
+	`created_date`
 ) VALUES (
-    4,
-    3,
-    "2019-12-01"
+	4,
+	3,
+	"2019-12-01"
 );
 
 INSERT INTO `review` (
-    `title`,
-    `description`,
-    `meal_id`,
-    `stars`,
-    `created_date`
+	`title`,
+	`description`,
+	`meal_id`,
+	`stars`,
+	`created_date`
 ) VALUES (
-    "Expensive",
-    "Taste was ok but they charge too much for an oat meal",
-    3,
-    2,
-    NOW()
+	"Expensive",
+	"Taste was ok but they charge too much for an oat meal",
+	3,
+	2,
+	NOW()
 );
 
 INSERT INTO `review` (
-    `title`,
-    `description`,
-    `meal_id`,
-    `stars`,
-    `created_date`
+	`title`,
+	`description`,
+	`meal_id`,
+	`stars`,
+	`created_date`
 ) VALUES (
-    "Nice",
-    "We liked the taste of everything",
-    2,
-    4,
-    NOW()
+	"Nice",
+	"We liked the taste of everything",
+	2,
+	4,
+	NOW()
 );
 
 INSERT INTO `review` (
-    `title`,
-    `description`,
-    `meal_id`,
-    `stars`,
-    `created_date`
+	`title`,
+	`description`,
+	`meal_id`,
+	`stars`,
+	`created_date`
 ) VALUES (
-    "Amazing",
-    "The best brunch I have ever had",
-    1,
-    5,
-    NOW()
+	"Amazing",
+	"The best brunch I have ever had",
+	1,
+	5,
+	NOW()
 );
 
 INSERT INTO `review` (
-    `title`,
-    `description`,
-    `meal_id`,
-    `stars`,
-    `created_date`
+	`title`,
+	`description`,
+	`meal_id`,
+	`stars`,
+	`created_date`
 ) VALUES (
-    "Average",
-    "It was alright but maybe I had bigger expectations and it couldn't fulfill them.",
-    2,
-    3,
-    NOW()
+	"Average",
+	"It was alright but maybe I had bigger expectations and it couldn't fulfill them.",
+	2,
+	3,
+	NOW()
 );
 
 -- [FUNCTIONALITY]
