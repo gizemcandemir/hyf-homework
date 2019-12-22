2f1c35bf555e88d55abcd6c5272b9cd26241132d
Gizem Candemir <gizemc@gmail.com>
2019-12-22 14:06:04 +0100

    Update subtract function

--- nodejs/week3/warmup/index.js
+++ nodejs/week3/warmup/index.js
@@ -1,9 +1,49 @@
 const express = require('express');
-const port = 3000;
+// const { PORT } = process.env || 5000;
+const PORT = 3000;
+
 const app = express();
 
-app.get("/", (req, res)=> {
-  console.log(req.query);
+app.get('/calculator', (req, res) => {
+	const queryData = [].concat.apply([], Object.values(req.query));
+	const data = queryData.map(Number);
+	res.send(`Query params: ${data}`);
 });
 
-app.listen(port, ()=> console.log(`Server started at ${port}`));
+app.get('/calculator/add', (req, res) => {
+	const queryData = [].concat.apply([], Object.values(req.query));
+	const data = queryData.map(Number);
+	console.log(queryData);
+	console.log(data);
+	const result = data.reduce((sum, value) => (sum = sum + value), 0);
+	res.send(`Query params: ${queryData} and the result is: ${result}`);
+})
+
+app.get('/calculator/subtract', (req, res) => {
+	const queryData = [].concat.apply([], Object.values(req.query));
+	const data = queryData.map(Number);
+	console.log(queryData);
+	console.log(data);
+	const result = data.reduce((sum, value) => (sum = - sum - value), 0 );
+	res.send(`Query params: ${queryData} and the result is: ${result}`);
+})
+
+app.get('/calculator/multiply', (req, res) => {
+	const queryData = [].concat.apply([], Object.values(req.query));
+	const data = queryData.map(Number);
+	console.log(queryData);
+	console.log(data);
+	const result = data.reduce((sum, value) => (sum = sum * value), 1);
+	res.send(`Query params: ${queryData} and the result is: ${result}`);
+})
+
+app.get('/calculator/divide', (req, res) => {
+	const queryData = [].concat.apply([], Object.values(req.query));
+	const data = queryData.map(Number);
+	console.log(queryData);
+	console.log(data);
+	const result = data.reduce((sum, value) => (sum = sum / value), 1);
+	res.send(`Query params: ${queryData} and the result is: ${result}`);
+})
+
+app.listen(PORT, ()=> console.log(`Server started at ${PORT}`));
