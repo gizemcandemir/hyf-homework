1c6acc2655d41ca490a419c06dda5b6270a24c68
Gizem Candemir <gizemc@gmail.com>
2020-01-11 03:00:17 +0100

    Add basic http methods

--- nodejs/week3/mealsharing/src/backend/api/meals.js
+++ nodejs/week3/mealsharing/src/backend/api/meals.js
@@ -3,13 +3,78 @@ const app = express();
 const router = express.Router();
 const pool = require("./../database");
 
-router.get("/", (request, response) => {
-  pool.query("some sql query", function(error, results, fields) {
-    response.json({});
-    // error will be an Error if one occurred during the query
-    // results will contain the results of the query
-    // fields will contain information about the returned results fields (if any)
+// GET
+router.get("/", (req, res) => {
+  pool.query("SELECT * from meals", function(error, results, fields) {
+    res.json(results);
   });
 });
+
+// POST
+router.post('/', (req, res) => {
+  let {
+    title,
+    maxNumberOfGuests,
+    description,
+    createdAt,
+    price
+  } = req.body;
+  const meal = {
+    title: title,
+    maxNumberOfGuests: Number(maxNumberOfGuests),
+    description: description,
+    createdAt: "20200202",
+    price: Number(price),
+  };
+  pool.query("INSERT INTO meals SET ?", meal, (error, results, fields) => {
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      res.send("Saved");
+    }
+  });
+});
+
+// GET by id
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  pool.query (`SELECT * FROM meals WHERE id = ${id}`,function(error, results, fields) {
+    if (error) {
+      return res.status(500).send(error);
+    }
+    res.json(results);
+  });
+})
+
+// PUT by id
+router.put('/:id', (req, res) => {
+  const idFromQuery = req.params.id;
+  pool.query ('UPDATE meals SET title=?, description=?,price=? where id=?',
+  [
+    req.body.title, 
+    req.body.description,
+    req.body.price,
+    Number(idFromQuery)
+  ], function(error, results, fields) {
+    if (error) {
+      return res.send(error);
+    }
+    res.send(`Meal with id ${idFromQuery} is updated`);
+  });
+})
+
+// DELETE
+router.put('/:id', (req, res) => {
+  const idFromQuery = req.params.id;
+  pool.query (`DELETE FROM meals WHERE id = ${id}`, function(error, results, fields) {
+    if (error) {
+      return res.send(error);
+    }
+    res.send(`Meal with id ${idFromQuery} is deleted`);
+  });
+})
+
+// GET api/meals/ query parameters
+
 
 module.exports = router;
--- nodejs/week3/mealsharing/src/backend/index.js
+++ nodejs/week3/mealsharing/src/backend/index.js
@@ -1,33 +1,22 @@
 const express = require("express");
 const app = express();
-const mealsRouter = require("./api/meals");
 const router = express.Router();
+const mealsRouter = require("./api/meals");
 
-const port = process.env.PORT || 3000;
-// For week4 no need to look into this!
-/* const path = require("path"); */
-/* // Serve the built client html
-const buildPath = path.join(__dirname, "../../dist");
-app.use(express.static(buildPath)); */
+const port = process.env.PORT || 5000;
 
 // Parse URL-encoded bodies (as sent by HTML forms)
 app.use(express.urlencoded({ extended: true }));
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
-router.use("/meals", mealsRouter);
-
+app.use("/api/meals", mealsRouter);
 app.use("/api", router);
 
-// For week4 no need to look into this!
-/* // Ensures that the client router works on reload aswell.
-// Sends all requests back to index.html where the routing lib takes over
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./../../dist/index.html"), function(err) {
-    if (err) {
-      res.status(500).send(err);
-    }
-  });
-}); */
+app.get("/", (req, res) => {
+  res.send("Mealsharing");
+});
 
-app.listen(port, () => console.log(`Server listening on port ${port}!`));
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}...`);
+});
