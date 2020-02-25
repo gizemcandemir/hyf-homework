245cc6247e04b070f86eebaa0a5022c623b2b62d
Gizem Candemir <gizemc@gmail.com>
2020-02-25 02:42:08 +0100

    Initial commit

---
+++ react/week5/Firebase-React-Express/admin/src/admin.js
@@ -0,0 +1,11 @@
+
+const serviceAccount = require("../config/private-admin-key.json");
+const admin = require("firebase-admin");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://react-w5-exercise-1.firebaseio.com"
+  });
+
+module.exports = admin;
+  
